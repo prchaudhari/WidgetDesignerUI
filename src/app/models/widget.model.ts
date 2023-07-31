23fd0bd1b8afbe90dcb92f3dc6a007ae0b4516d8
prchaudhari <chaudhari.pallavi@gmail.com>
2023-07-31 15:38:00 +0530

    entity changes

--- src/app/models/widget.model.ts
+++ src/app/models/widget.model.ts
@@ -3,6 +3,9 @@ export interface Widget {
   widgetName: string,
   description: string,
   dataSourceJson: string,
+  widgetCSS: string;
+  widgetCSSUrl: string;
+  widgetIcon: string;
   initialWidth: number
   initialHeight: number
 }
