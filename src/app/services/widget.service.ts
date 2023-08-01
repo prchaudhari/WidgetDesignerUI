01e8f73deaa8fea2e4ea4ca0c97a4f2e9a739fff
mukul rai <mukulrai141966@gmail.com>
2023-08-01 16:47:41 +0530

    .

--- src/app/services/widget.service.ts
+++ src/app/services/widget.service.ts
@@ -6,30 +6,30 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class WidgetsService {
+export class WidgetService {
   baseApiUrl: string = "https://localhost:44381";
 
   constructor(private http: HttpClient) { }
 
-  getAllWidgets(): Observable<Widget[]> {
-    return this.http.get<Widget[]>(this.baseApiUrl + '/api/widgets');
+  getAllWidget(): Observable<Widget[]> {
+    return this.http.get<Widget[]>(this.baseApiUrl + '/api/widget');
   }
 
   addWidget(newWidget: Widget): Observable<Widget> {
     newWidget.id = '00000000-0000-0000-0000-000000000000';
-    return this.http.post<Widget>(this.baseApiUrl + '/api/widgets', newWidget);
+    return this.http.post<Widget>(this.baseApiUrl + '/api/widget', newWidget);
   }
 
   getWidget(id: string): Observable<Widget> {
-    return this.http.get<Widget>(this.baseApiUrl + '/api/widgets/' + id);
+    return this.http.get<Widget>(this.baseApiUrl + '/api/widget/' + id);
   }
 
   updateWidget(id: string, updateWidgetRequest: Widget): Observable<Widget> {
-    return this.http.put<Widget>(this.baseApiUrl + '/api/widgets/' + id, updateWidgetRequest);
+    return this.http.put<Widget>(this.baseApiUrl + '/api/widget/' + id, updateWidgetRequest);
   }
 
   deleteWidget(id: string): Observable<Widget> {
-    return this.http.delete<Widget>(this.baseApiUrl + '/api/widgets/' + id);
+    return this.http.delete<Widget>(this.baseApiUrl + '/api/widget/' + id);
   }
 
 }
